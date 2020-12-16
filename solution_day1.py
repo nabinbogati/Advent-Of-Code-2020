a4be7cace97ab6cff754c8c611499a0ee928b647
nabinbogati <nabin31bogati@gmaill.com>
2020-12-16 22:39:23 +0545

    day1 puzzle solution

---
+++ solution_day1.py
@@ -0,0 +1,56 @@
+"""
+    Advent of Code Solution To Day1 Problem
+    For example, suppose your expense report contained the following:
+
+    1721
+    979
+    366
+    299
+    675
+    1456
+
+    In this list, the two entries that sum to 2020 are 1721 and 299.
+    Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+"""
+DATA = []
+
+def read_file(filename):
+    """ reads each line from given file add adds to list """
+    with open(filename, 'r') as file:
+        for number in file.readlines():
+            DATA.append(int(number))
+
+
+def calculate_two_sum():
+    """ returns multiplication of two numbers that adds up to 2020 from given list """
+    for value in DATA:
+        if 2020-value in DATA:
+            result = value*(2020-value)
+            break
+
+    return result
+
+def calculate_three_sum():
+    """ returns multiplication of three numbers that adds up to 2020 from given list """
+    for first in DATA:
+        for second in DATA:
+            if 2020-(first+second) in DATA:
+                result = first*second*(2020-(first+second))
+                break
+
+    return result
+
+
+def main():
+    """ main function from where program execution begains """
+    filename = 'input_day1.txt'
+    read_file(filename)
+    three_sum = calculate_two_sum()
+    two_sum = calculate_three_sum()
+
+    print(three_sum, two_sum)
+
+
+if __name__ == "__main__":
+    main()
